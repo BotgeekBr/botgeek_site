7d268f02bea90a083cb3a06ccd1fc1a9b6ffc083
FFsolutec <ffsolutec@gmail.com>
2023-08-05 09:31:03 -0300

    update

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {Box, Divider, IconButton, Typography} from "@mui/material"
+import React, { useEffect, useState } from "react";
+import {Box, Divider, Grid, IconButton, Typography} from "@mui/material"
 import { alpha,styled, useTheme } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
@@ -11,16 +11,28 @@ import backgroundImagePageTthee from "../../assets/programming-background-with-p
 import backgroundImagePageFour from "../../assets/future-technology-processing-business-background-with-abstract-circuit-boards.jpg"
 
 
+
+
 //LOGOHEADERSECTIONS
 import BootGeek from "../../assets/BOT-0001-23 Brand_VF-03.png"
 
 //HEADERTEXTIMAGE
 import HeaderText from '../../assets/Novo_Projeto__10_-removebg-preview.png'
 
-const BoxInicio= styled(Box)(({ theme }) => ({
+const backgroundImageArray = [
+  backgroundImagePageOne,
+  backgroundImagePageTwo,
+  backgroundImagePageTthee,
+  backgroundImagePageFour
+];
+
+
+
+
+const BoxInicio= styled(Box)(({ theme,backgroundImage  }) => ({
     width: "100%",
     height: "100vh",
-    backgroundImage: `url(${backgroundImagePageOne})`,
+    backgroundImage: `url(${backgroundImage})`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
@@ -31,7 +43,10 @@ const BoxInicio= styled(Box)(({ theme }) => ({
     marginTop: "-30px",
     [theme.breakpoints.down('sm')]: {
       marginTop: "0px",
-    }
+      flexDirection:"column"
+    },
+    opacity: "fadeInOut ? 1 : 0", // Inicia com a opacidade definida pelo fadeInOut
+    transition: "opacity 0.5s ease-in-out", // Adiciona a transição
   }));
   
 const SectionOne = styled(Box)(({ theme }) => ({
@@ -48,7 +63,7 @@ const SectionOne = styled(Box)(({ theme }) => ({
   backdropFilter: "blur(10px)",
   [theme.breakpoints.down('sm')]: {
     zIndex: 999,
-    maxWidth: "80%",
+    maxWidth: "100%",
     marginTop: "0px",
   }
 }));
@@ -58,110 +73,175 @@ const SectionTwo = styled(Box)(({ theme }) => ({
   justifyContent: "center",
   alignItems: "center",
   padding: 10,
- 
+  [theme.breakpoints.down('sm')]: {
+   
+    flexDirection:"row"
+  }
 }));
 
 
+const ButtonGroupList = styled(ButtonGroup)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection:"column",
+  backgroundColor: alpha( "#0D120B", 0.6),
+              padding: 1,
+              backdropFilter: "blur(10px)",
+              borderRadius: 3,
+              [theme.breakpoints.down('sm')]: {
+   
+                flexDirection:"row"
+              }
+}));
+
+
+
 const BoxContext = styled(Box)(({ theme }) => ({
-  width: "100%", maxWidth: 500, textAlign: "left",paddingLeft: "15%", paddingRight: "15%",
+  width: "100%", maxWidth: 400, textAlign: "left",paddingLeft: "15%", paddingRight: "15%",
+  [theme.breakpoints.down('lg')]: {
+    maxWidth: 300
+  },
   [theme.breakpoints.down('sm')]: {
     maxWidth: 250
   }
  
 }));
 
-const buttons = [
-  <Button key="three" sx={{mb: 1,background: "none", border: `1px solid ${alpha( "#0D120B", 0.4)}`, backgroundColor: "none" , borderColor: "none", color: "white", fontWeight: 700}}>01</Button>,
-  <Button key="three" sx={{mb: 1,background: "none", border: `1px solid ${alpha( "#0D120B", 0.4)}`, backgroundColor: "none" , borderColor: "none", color: "white", fontWeight: 700}}>02</Button>,
-  <Button key="three" sx={{mb: 1,background: "none", border: `1px solid ${alpha( "#0D120B", 0.4)}`, backgroundColor: "none" , borderColor: "none", color: "white", fontWeight: 700}}>03</Button>,
-  <Button key="three" sx={{mb: 1,background: "none", border: `1px solid ${alpha( "#0D120B", 0.4)}`, backgroundColor: "none" , borderColor: "none", color: "white", fontWeight: 700}}>04</Button>,
-];
+
 
   
 
 function Inicio() {
+ 
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [fadeInOut, setFadeInOut] = useState(true);
+
+  const buttons = [
+    <Button key="one" onClick={() => setCurrentImageIndex(0)} sx={{ mb: 1, background: "none", border: `1px solid ${alpha( "#0D120B", 0.4)}`, backgroundColor: "none", borderColor: "none", color: "white", fontWeight: 700 }}>01</Button>,
+    <Button key="two" onClick={() => setCurrentImageIndex(1)} sx={{ mb: 1, background: "none", border: `1px solid ${alpha( "#0D120B", 0.4)}`, backgroundColor: "none", borderColor: "none", color: "white", fontWeight: 700 }}>02</Button>,
+    <Button key="three" onClick={() => setCurrentImageIndex(2)} sx={{ mb: 1, background: "none", border: `1px solid ${alpha( "#0D120B", 0.4)}`, backgroundColor: "none", borderColor: "none", color: "white", fontWeight: 700 }}>03</Button>,
+    <Button key="four" onClick={() => setCurrentImageIndex(3)} sx={{ mb: 1, background: "none", border: `1px solid ${alpha( "#0D120B", 0.4)}`, backgroundColor: "none", borderColor: "none", color: "white", fontWeight: 700 }}>04</Button>,
+  ];
+  
+  useEffect(() => {
+    const intervalTime = 5000; // Defina o tempo em milissegundos (5 segundos)
+    const interval = setInterval(() => {
+      setFadeInOut(false); // Inicia a transição para ocultar a imagem
+      setTimeout(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % backgroundImageArray.length);
+        setFadeInOut(true); // Inicia a transição para exibir a nova imagem
+      }, 500); // Tempo suficiente para ocultar a imagem completamente (deve ser igual ao tempo de transição CSS)
+    }, intervalTime);
+
+    return () => clearInterval(interval); // Limpa o intervalo quando o componente é desmontado
+
+  }, []);
 
   const theme = useTheme()
   return (
     <div name="inicio" id="inicio">
-      <BoxInicio >
-      <SectionOne>
-      
-          <Box paddingTop={6} sx={{paddingLeft: "15%", paddingRight: "15%"}} >
-          <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-              
-                color="inherit"
-         
-              >
-              <Box
-                component="img"
-                  sx={{
-                    height: "100%",
-                    width: "100%",
+      <BoxInicio backgroundImage={backgroundImageArray[currentImageIndex]}>
+        <SectionOne>
+        
+            <Grid container xs={12}>
+              <Box paddingTop={6} sx={{paddingLeft: "15%", paddingRight: "15%"}} >
+              <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
                   
-                    maxWidth: { xs: 250, md: 250,sm:250 },
-                    
-                  }}
-                alt="The house from the offer."
-                src={BootGeek}
-              />
-              </IconButton>
-          </Box>
+                    color="inherit"
+            
+                  >
+                  <Box
+                    component="img"
+                      sx={{
+                        height: "100%",
+                        width: "100%",
+                      
+                        maxWidth: { xs: 250, md: 250,sm:250 },
+                        
+                      }}
+                    alt="The house from the offer."
+                    src={BootGeek}
+                  />
+                  </IconButton>
+              </Box>
 
-          <BoxContext style={stylesDivsOne}>
-          <Box
-              component="img"
-              sx={{
-                height: "100%",
-                width: "100%",
-                maxHeight: { xs: 450, md: 450 },
-                maxWidth: { xs: 550, md: 650,sm:250 },
-                
-              }}
-              alt="The house from the offer."
-              src={HeaderText}
-            />
-          </BoxContext>
+              
 
-          <BoxContext style={stylesDivsTwo}>
-            <Typography style={TextConteudo}>
-            Formado por engenheiros de software a Botgeek
-            veio para facilitar o desenvolvimento de software.
-            Entendemos a base de um bom software é o
-            relacionamento com nossos clientes.
-            </Typography>
-          </BoxContext>
-            <Divider sx={{color: "#C4F015",background: "#C4F015", width: "80%",marginLeft: "15%", marginBottom: "5%", marginTop: "5%"}}></Divider>
-            <BoxContext style={stylesDivsThree}>
-            <Typography  style={TextLastConteudo}>Nos estamos a mais de 7 anos no
-mercado de tecnologia inovando e
-construindo sonhos.</Typography>
-          </BoxContext>
-       
-      </SectionOne>
-      <SectionTwo sx={{
-    
-    
-  }}> 
-        <ButtonGroup
-          orientation="vertical"
-          aria-label="vertical contained button group"
-          variant="outlined"
-          sx={{ 
-            backgroundColor: alpha( "#0D120B", 0.6),
-            padding: 1,
-            backdropFilter: "blur(10px)",
-            borderRadius: 3
-        }}
-        >
-          {buttons}
-        </ButtonGroup>  
-      </SectionTwo>
-    </BoxInicio>
+              
+              
+              <Grid item xs={2}></Grid>
+                <Grid item xl={8} lg={8} md={8} xs={12}>
+                  <BoxContext style={stylesDivsOne}>
+                <Box
+                      component="img"
+                      sx={{
+                        height: "100%",
+                        width: "100%",
+                        maxHeight: { xs: 450, md: 450 },
+                        maxWidth: { xs: 550, md: 650,sm:250 },
+                        
+                      }}
+                      alt="The house from the offer."
+                      src={HeaderText}
+                    />
+                  </BoxContext>
+                </Grid>
+              <Grid item xs={2}></Grid>
+
+
+
+
+              <Grid item xs={2}></Grid>
+                <Grid item xl={8} lg={8} md={8} xs={12}>
+                <BoxContext style={stylesDivsTwo}>
+                <Typography style={TextConteudo}>
+                Formado por engenheiros de software a Botgeek
+                veio para facilitar o desenvolvimento de software.
+                Entendemos a base de um bom software é o
+                relacionamento com nossos clientes.
+                </Typography>
+              </BoxContext>
+                </Grid>
+              <Grid item xs={2}></Grid>
+
+
+              <Grid item xs={12}><Divider sx={{color: "#C4F015",background: "#C4F015", width: "60%",marginLeft:"10%"}}></Divider></Grid>
+
+
+              
+                <Grid item xl={8} lg={8} md={8} xs={12}>
+                <BoxContext style={stylesDivsThree}>
+                <Typography  style={TextLastConteudo}>Nos estamos a mais de 7 anos no
+    mercado de tecnologia inovando e
+    construindo sonhos.</Typography>
+              </BoxContext>
+                </Grid>
+              <Grid item xs={2}></Grid>
+
+
+
+          
+            </Grid>
+        </SectionOne>
+        <SectionTwo sx={{
+      
+      
+    }}> 
+          <ButtonGroupList
+            
+            variant="outlined"
+            
+          >
+            {buttons}
+          </ButtonGroupList>  
+        </SectionTwo>
+      </BoxInicio>
     </div>
   );
 }
@@ -170,11 +250,11 @@ export default Inicio;
 
 
 const stylesDivsOne = {
-  paddingTop: "8%"
+ 
 }
 
 const stylesDivsTwo = {
-  paddingTop: "3%"
+ 
 }
 const stylesDivsThree = {
  
@@ -182,9 +262,9 @@ const stylesDivsThree = {
 const TextConteudo = {
   color:"white",
   fontWeight: 500,
-  letterSpacing: 1.8,
-  lineHeight: 2,
-  wordSpacing: 2,
+  letterSpacing: 1.5,
+  lineHeight: 1.5,
+  wordSpacing: 1.5,
   fontSize: 15
 }
 
